011cd4c3085a7b6c913ce4edeb04cdf539787f2a
macbook <haoran.liu@intelligentgroup.cn>
2017-02-14 16:44:28 +0800

    添加注释说明

--- scarybug/scarybug/AppDelegate.m
+++ scarybug/scarybug/AppDelegate.m
@@ -10,7 +10,8 @@
 #include "MasterViewcontroller.h"
 #import "ScaryBugDoc.h"
 @interface AppDelegate ()
-@property (nonatomic,strong) IBOutlet MasterViewcontroller *masterViewController;
+@property (nonatomic,strong) IBOutlet MasterViewcontroller *masterViewController;//向appdelegate声明view的存在
+//接下来进行实例化
 @property (weak) IBOutlet NSWindow *window;
 @end
 
@@ -29,15 +30,21 @@
     ScaryBugDoc *bug3 = [[ScaryBugDoc alloc] initWithTitle:@"Wolf Spider" rating:5 thumbImage:[NSImage imageNamed:@"wolfSpiderThumb.jpg"] fullImage:[NSImage imageNamed:@"wolfSpider.jpg"]];
     ScaryBugDoc *bug4 = [[ScaryBugDoc alloc] initWithTitle:@"Lady Bug" rating:1 thumbImage:[NSImage imageNamed:@"ladybugThumb.jpg"] fullImage:[NSImage imageNamed:@"ladybug.jpg"]];
     NSMutableArray *bugs = [NSMutableArray arrayWithObjects:bug1, bug2, bug3, bug4, nil];
-    
-    self.masterViewController.bugs = bugs;
-    // 2. Add the view controller to the Window's content view
+    //初始化数据
+    self.masterViewController.bugs = bugs;//将数据存入bugs字典中
+    // 2. Add the view controller to the Window's content view将view controller加入窗口内容
     [self.window.contentView addSubview:self.masterViewController.view];
     self.masterViewController.view.frame = ((NSView*)self.window.contentView).bounds;
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+  
+   
 }
-
+- (void)windowWillClose:(NSNotification *)notification
+{
+   
+    [NSApp terminate:nil];
+}
 @end
--- scarybug/scarybug/MasterViewcontroller.m
+++ scarybug/scarybug/MasterViewcontroller.m
@@ -30,16 +30,18 @@
     [super viewDidLoad];
     // Do view setup here.
 }
-- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row   //此方法用于表示view中每行每列所显示的内容。
+{
     
     // Get a new ViewCell
     NSTableCellView *cellView = [tableView makeViewWithIdentifier:tableColumn.identifier owner:self];
-    
+    //通过makeViewWithIdentifier 得到一个cellView。这里会创建一个基于tableColumn的identifier的cellView。
     // Since this is a single-column table view, this would not be necessary.
     // But it's a good practice to do it in order by remember it when a table is multicolumn.
-    if( [tableColumn.identifier isEqualToString:@"Bugcolumn"] )
+    if( [tableColumn.identifier isEqualToString:@"Bugcolumn"] )//Bugcolumn是设置的列标识，这里table view只有一列
     {
         ScaryBugDoc *bugDoc = [self.bugs objectAtIndex:row];
+        //对象bugDoc 是scaryBugDOC类中字典bugs相对每行的内容，bugs字典中已经声明存下了虫子数据。
         cellView.imageView.image = bugDoc.thumbImage;
         cellView.textField.stringValue = bugDoc.data.title;
         return cellView;
@@ -70,7 +72,7 @@
         return selectedBug;
     }
     return nil;
-    
+    //整数对象selectedRow 是tableView中选择的行数。若该行存在，则将该行的数值在字典bugs对象返回。
 }
 
 -(void)setDetailInfo:(ScaryBugDoc*)doc
@@ -87,10 +89,12 @@
     [self.bugTitleView setStringValue:title];
     [self.bugImageView setImage:image];
     [self.bugRating setRating:rating];
-    
+    //将字典中虫子的信息分别显示在viewController的textfield，imageView控件中。
 }
 
 - (void)tableViewSelectionDidChange:(NSNotification *)aNotification
+// First, you need to know which row is selected. The table view tells its delegate that a row is selected by calling ‘tableViewSelectionDidChange’. So you need to implement that method in our view controller to receive that notification.
+//首先应该知道哪行被选择了，使view controller 接收行被选择的消息。
 {
     ScaryBugDoc *selectedDoc = [self selectedBugDoc];
     
@@ -105,16 +109,16 @@
 }
 
 - (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
-    return [self.bugs count];
+    return [self.bugs count];//表示表格中需要显示多少行内容。
 }
 -(IBAction)addRows:(id)sender{
     // 1. Create a new ScaryBugDoc object with a default name
     ScaryBugDoc *newDoc = [[ScaryBugDoc alloc] initWithTitle:@"New Bug" rating:0.0 thumbImage:nil fullImage:nil];
-    //add new bug object to our model dictionary.
-    [self.bugs addObject:newDoc];
+    //add new bug object to our model dictionary.c创建新的bug对象
+    [self.bugs addObject:newDoc];//将新的bug对象加入bugs词典中。
     NSInteger newRowIndex = self.bugs.count-1;
     //Insert new Row in the table view.
-    [self.bugsTableView insertRowsAtIndexes:[NSIndexSet indexSetWithIndex:newRowIndex] withAnimation:NSTableViewAnimationEffectGap];
+    [self.bugsTableView insertRowsAtIndexes:[NSIndexSet indexSetWithIndex:newRowIndex] withAnimation:NSTableViewAnimationEffectGap];//最后是确保新的行的界限可被拖动的操作。
     //select the new bug and scroll to make sure it's visible.
     [self.bugsTableView selectRowIndexes:[NSIndexSet indexSetWithIndex:newRowIndex] byExtendingSelection:NO];
     [self.bugsTableView scrollRowToVisible:newRowIndex];
